cd9b706f6dc9dd74ddea109481b5f00bc0590e0f
lhalf <80602159+lhalf@users.noreply.github.com>
2026-01-28 22:40:06 +0000

    refactor sender app

--- import/http/sender/src/app.rs
+++ import/http/sender/src/app.rs
@@ -3,7 +3,10 @@ use crate::{listener, server};
 
 pub async fn run() -> anyhow::Result<()> {
     let config = Config::try_load()?;
+    spawn_tasks(config).await
+}
 
+async fn spawn_tasks(config: Config) -> anyhow::Result<()> {
     let mut tasks = tokio::task::JoinSet::new();
 
     tasks.spawn(server::run(config.clone()));
